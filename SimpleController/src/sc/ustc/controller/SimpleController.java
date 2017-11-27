a697d04b0f804e2793ec2532fe763562f1732034
NageNalock <83773602@qq.com>
2017-11-27 22:59:25 +0800

    无法依赖SC跳转

---
+++ SimpleController/src/sc/ustc/controller/SimpleController.java
@@ -0,0 +1,27 @@
+package sc.ustc.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class SimpleController extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        OutputStream out = 
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/HelloJ2EE.html").forward(req,resp);
+    }
+}
