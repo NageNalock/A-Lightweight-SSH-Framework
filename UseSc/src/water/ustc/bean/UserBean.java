d6d05d8ad5b8e81dfcf91f7fab6d00f278d8514e
NageNalock <83773602@qq.com>
2018-01-08 13:40:39 +0800

    E6
    更加优雅

--- UseSc/src/water/ustc/bean/UserBean.java
+++ UseSc/src/water/ustc/bean/UserBean.java
@@ -23,43 +23,27 @@ public class UserBean  {
         String userPassFromDB = null; // 查询出的密码
         // 1.执行查询
         QueryResult queryResult = conversation.query(this, "userName");
-        // 2.验证结果是否为懒加载
-        if (queryResult.isLazy())
+
+        // 2.获取密码(不用考虑懒加载
+        ArrayList<HashMap<String, String>> resultList = queryResult.getResult();
+        for(HashMap<String,String> result:resultList)
         {
-            // 是懒加载
-            System.out.println("signIn得到的查询结果是懒加载");
-            // 2.1 执行懒加载方法获得结果
-            ArrayList<HashMap<String, String>> hashMapsList = queryResult.notLazy();
-            for(HashMap<String,String> hashMap:hashMapsList)
-            {
-                userPassFromDB = hashMap.get("userPass"); // 反正这个情况只会有一个值
-                System.out.println("数据库中读出的密码为:"+userPassFromDB);
-            }
-        }else {
-            // 不是懒加载
-            System.out.println("signIn得到的查询结果不是懒加载");
-            // 2.1
-            ArrayList<HashMap<String, String>> resultList = queryResult.getResultList();
-            for(HashMap<String,String> hashMap:resultList)
-            {
-                userPassFromDB = hashMap.get("userPass"); // 反正这个情况只会有一个值
-                System.out.println("数据库中读出的密码为:"+userPassFromDB);
-            }
+            userPassFromDB = result.get("userPass"); // 反正这个情况只会有一个值
+            System.out.println("数据库中读出的密码为:"+userPassFromDB);
         }
+
         // 3.匹配密码
-        if (userPass.equals(userPassFromDB))
-            return true;  // 相等
-        else return false;
+        return userPass.equals(userPassFromDB);
     }
 
     public boolean signUp()
     {
         // 1. Conversation
         Conversation conversation = new Conversation(this.getClass().getName());
-        // 2. 调用insert()
-        boolean insertResult = conversation.insert(this);
+//        // 2. 调用insert()
+//        boolean insertResult = conversation.insert(this);
         // 3. 得到结果()
-        return insertResult;
+        return conversation.insert(this);
     }
 
 
